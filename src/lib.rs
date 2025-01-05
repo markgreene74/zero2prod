199d7923c23718da6b781c2e21995a6d52169f62
markgreene74 <markgreene74@users.noreply.github.com>
2025-01-05 15:37:47 +0000

    replace address with a TcpListener
    ... which allows a lot more flexibility in
    running the tests.
    
    Have left a few commented out DEBUG lines on purpose
    as a reminder for myself.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 
 use actix_web::dev::Server;
 use actix_web::{get, web, App, HttpServer, Responder};
+use std::net::TcpListener;
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
@@ -13,9 +14,9 @@ async fn health_check() -> impl Responder {
     "OK"
 }
 
-pub fn run(address: &str) -> std::io::Result<Server> {
+pub fn run(listener: TcpListener) -> std::io::Result<Server> {
     let server = HttpServer::new(|| App::new().service(greet).service(health_check))
-        .bind(address)?
+        .listen(listener)?
         .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,26 +1,32 @@
 //! tests/health_check.rs
 
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[actix_web::test]
 async fn health_check_works() {
     // start the application
-    spawn_app();
+    let address = spawn_app();
+    // dbg!(&address); // DEBUG
 
     // use reqwest to perform HTTP requests against the application
     let client = reqwest::Client::new();
     let response = client
-        .get("http://127.0.0.1:8080/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
 
     // asserts go here
+    // dbg!(&response); // DEBUG
     assert!(response.status().is_success());
     assert_eq!(Some(2), response.content_length());
 }
 
-fn spawn_app() {
-    let server = run("127.0.0.1:8080").expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
     let _ = actix_web::rt::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
