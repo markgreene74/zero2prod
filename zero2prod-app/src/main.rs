6a6d89407ccfcbd538c09e11ffcbb0334202b7f8
markgreene74 <markgreene74@users.noreply.github.com>
2025-01-07 23:37:59 +0000

    basic Dockerfile and compose

--- zero2prod-app/src/main.rs
+++ zero2prod-app/src/main.rs
@@ -5,6 +5,6 @@ use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind port 8080");
+    let listener = TcpListener::bind("0.0.0.0:8080").expect("Failed to bind port 8080");
     run(listener)?.await
 }
